1234aa7849dbaaaba9fbbc76ef2194659b52c017
Céline DEVOS <140008750+Nelicee@users.noreply.github.com>
2024-08-18 15:15:09 +0200

    installation de Symfony - Premieres pages - Moteur de template twig - ORM doctrine

---
+++ migrations/Version20240818085839.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240818085839 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipe DROP FOREIGN KEY FK_DA88B13712469DE2');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP INDEX IDX_DA88B13712469DE2 ON recipe');
+        $this->addSql('ALTER TABLE recipe DROP category_id, DROP duration, DROP thumbnail');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, slug VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE recipe ADD category_id INT DEFAULT NULL, ADD duration INT DEFAULT NULL, ADD thumbnail VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE recipe ADD CONSTRAINT FK_DA88B13712469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_DA88B13712469DE2 ON recipe (category_id)');
+    }
+}
---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(Request $request, RecipeRepository $repository): Response
+    {
+      $recipes = $repository->findWithDurationLowerThan(20);
+        
+        return $this->render(
+            'recipe/index.html.twig',
+            [
+                'recipes' => $recipes
+            ]
+        );
+    }
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z-0-9-]+'])]
+    public function show(Request $request, string $slug,  int $id,  RecipeRepository $repository): Response
+    {
+        $recipe = $repository->find($id);
+        if ($recipe->getSlug() !== $slug){
+            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getslug(), 'id'=> $recipe->getId()]);
+        }
+        return $this->render('recipe/show.html.twig', [
+          'recipe' => $recipe
+        ]);
+    }
+}
+        
